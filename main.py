f6e08e1e16e746d85348aa00f8c8a5142bc865c2
Teguh Permana 666 <teguhpermana096@gmail.com>
2022-09-17 13:00:45 +0800

    feature/add_mul_div

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from math_function import add
+from decimal import DivisionByZero
+from math_function import add,mul,div
 
 
 def main():
@@ -6,11 +7,22 @@ def main():
     data_1 = int(input("masukkan input 1 :"))
     data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
-
-    if operator == "+":
-        result = add(data_1, data_2)
-
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    try:
+        if operator == "+":
+            result = add(data_1, data_2)
+            
+        if operator == "*":
+            result = mul(data_1,data_2)
+        if operator == "/":
+            try:    
+                result = div(data_1,data_2)
+            except DivisionByZero:
+                print("ZeroDivisionError Occurred and Handle")
+    except NameError:
+        print("NameError Occurred and Handled")
+    finally:
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+        
 
 
 if __name__ == "__main__":
